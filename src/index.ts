a233ed0421604be997a78632c4e14bf4e8b076a6
bien-123 <bienle160720@gmail.com>
2023-10-27 00:33:50 +0700

    [F] Connect DB

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const db = require('./db/config');
 
 const app = express();
 const port = 3000;
@@ -7,6 +11,7 @@ app.get('/', (req: any, res: any) => {
   res.send('Hello World!');
 });
 
+db.connect();
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
